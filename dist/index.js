06b6d97f35c5e90b3a69b00f56e7972c666ecc08
Anndrei87 <andrei09ka6@gmail.com>
2023-09-20 22:38:14 -0300

    feat: init state project

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log("hello word");
-const number = 100;
-console.log('helllooo', number);
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-console.log("hello word");
-const number: number = 100;
-console.log('helllooo',number);
