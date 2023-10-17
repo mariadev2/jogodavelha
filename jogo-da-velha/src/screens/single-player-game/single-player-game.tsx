35e8ea6f8d888b5c1f9fa2f4bb0c882873867722
mariadev2 <pmariaeduarda987@gmail.com>
2023-10-17 17:03:31 -0300

    docs

--- jogo-da-velha/src/screens/single-player-game/single-player-game.tsx
+++ jogo-da-velha/src/screens/single-player-game/single-player-game.tsx
@@ -1,37 +1,72 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 import styles from "./single-player-game.styles";
 import { BackgroundPage } from "../../components";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Board } from '../../components'
-import {printFormateBoard, isEmpy,isFull,isTerminal,getAvailableMoves, BoardState} from '../../utils'
+import {getBestMove,printFormateBoard, isEmpy,isFull,isTerminal,getAvailableMoves, BoardState} from '../../utils'
 
 
 export default function SinglePlayerGame() : ReactElement{
+
     const [state, setState] = useState<BoardState>([
         null,null,null,
         null,null,null,
         null,null,null
+    ]);
+    const [turn,setTurn] = useState 
+    <"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT");
+    const [isHumanMaximizing, setIsHumanMaximizing] = useState
+    <boolean>(true);
+    
+    const gameResult = isTerminal(state);
 
-    ])
-    ///printFormateBoard(b);
-    //console.log(isTerminal(b));
-    //console.log(isEmpy(b));
-    //console.log(isFull(b));
-    //console.log(getAvailableMoves(b));
+    const insertCell = (cell: number, symbol:"x" | "o" ) : 
+    void => {
+            const stateCopy: BoardState = [...state];
+            if (stateCopy[cell] || isTerminal(stateCopy))
+            return;
+            stateCopy[cell] = symbol;
+            setState(stateCopy);
+    }
+
+
     const handleOnCellPressed = (cell: number): void => {
-        const stateCopy: BoardState = [...state];
-        if (stateCopy[cell] || isTerminal(stateCopy))
-        return;
-        stateCopy[cell] = "x";
-        setState(stateCopy);
+        if(turn != "HUMAN") return;
+        insertCell(cell,isHumanMaximizing ? "x" : "o");
+        setTurn("BOT");
     };
+        useEffect(() => {
+            if(gameResult){
+                alert("Game over");
+            }else{
+                if(turn == "BOT"){
+                    if(isEmpy(state)){
+                        const centerAndCorners = [0,2,6,8,4];
+                        const firstMove = centerAndCorners[Math.floor(Math.random() * centerAndCorners.length)]
+                        insertCell(firstMove, "x");
+                        setIsHumanMaximizing(false);
+                        setTurn("HUMAN");
+
+
+                    }else {
+                        const best = getBestMove(
+                                    state, !isHumanMaximizing,0,-1);
+
+                        insertCell(best, isHumanMaximizing ? "o" : "x");
+                        setTurn("HUMAN");
+
+                    }
+                }
+            }
+        }, [state, turn])
+
     return(
         <BackgroundPage>
             <SafeAreaView style= {styles.container}>
                 
                 <Board 
-                    disabled = {Boolean(isTerminal(state))}
+                    disabled = {Boolean(isTerminal(state)) || turn != "HUMAN" }
                     onCellPressed = {(cell) => {
                         handleOnCellPressed(cell)
                 }} 
@@ -44,4 +79,5 @@ export default function SinglePlayerGame() : ReactElement{
             </SafeAreaView>
         </BackgroundPage>
     )
+
 }
